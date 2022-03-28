26e8fb32f157aebc1a42a30a3f4c07c3ebfd1cc0
shouryakul <shourya.kulshrestha2000@gmail.com>
2022-03-28 21:51:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/TodoDao.kt
@@ -0,0 +1,22 @@
+package com.example.todoapp
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface TodoDao{
+
+    @Insert()
+    fun insertTask(todoModel: TodoModel):Long
+
+    @Query("SELECT * FROM TodoModel WHERE isFinished == 0")
+    fun getTask():LiveData<List<TodoModel>>
+
+    @Query("UPDATE TodoModel SET isFinished = 1 WHERE id =:uid")
+    fun finishTask(uid:Long)
+
+    @Query("DELETE FROM TodoModel WHERE id =:uid")
+    fun deleteTask(uid:Long)
+}
